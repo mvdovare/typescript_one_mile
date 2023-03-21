8f791f94719e4202aecad840a62742583105159e
mvdovare <80451333+mvdovare@users.noreply.github.com>
2023-03-21 19:56:28 +0200

    2 files modified

--- index.ts
+++ index.ts
@@ -2,9 +2,11 @@ import * as turf from '@turf/turf';
 import { geojsonToWKT } from '@terraformer/wkt';
 import './style.css';
 import { saveAs } from 'file-saver';
-import { LatLngExpression, LeafletMouseEvent } from 'leaflet';
 import L = require('leaflet');
 
+const latLngInput = document.getElementById('latLngInput');
+const radiusInput = document.getElementById('radiusInput');
+const refreshButton = document.getElementById('refreshButton');
 const multipolygonContainer: HTMLElement = document.getElementById(
   'multipolygonContainer'
 );
@@ -12,63 +14,84 @@ const geojsonContainer: HTMLElement =
   document.getElementById('geojsonContainer');
 const save_button: HTMLElement = document.getElementById('save_button');
 
-const fname = document.getElementById('fname') as HTMLInputElement;
+refreshButton.onclick = () => draw();
 
-const fname_value = fname?.value;
-console.log(fname_value); // 👉️ "Initial value"
+let map = L.map('map');
+let geojsonCircle: L.GeoJSON<any>;
+let circleMarker: L.Marker<any>;
 
-if (fname_value != null) {
-  console.log(fname.value); // 👉️ "Initial value"
+function draw(): void {
+  const latLngFromInput = (latLngInput as HTMLInputElement).value;
+  const [latString, lngString] = latLngFromInput.split(',');
+  const lat = +latString?.trim();
+  const lng = +lngString?.trim();
+
+  const radius = +(radiusInput as HTMLInputElement).value;
+
+  //38.631312109301305, -90.21014776165008
+
+  if (lat && lng && radius) {
+    // PASTE YOU COORDINATES, RADIUS and STEPS HERE
+    const steps = 100;
+
+    const input = {
+      lngLat: [lng, lat],
+      radius,
+    };
+    const options = { steps: steps, units: 'miles' };
+
+    const circle = turf.circle(input.lngLat, input.radius, options);
+    const geojson: GeoJSON.FeatureCollection<any> = {
+      type: 'FeatureCollection',
+      features: [circle],
+    };
+    const wkt: string = geojsonToWKT(geojson.features[0].geometry);
+    const multipolygon = wkt
+      .replace('POLYGON ((', 'MULTIPOLYGON (((')
+      .replace('))', ')))');
+
+    multipolygonContainer.innerHTML = multipolygon;
+    geojsonContainer.innerHTML = JSON.stringify(
+      geojson['features'][0]['geometry']['coordinates'][0]
+    );
+
+    save_button.onclick = () => {
+      const filename = window.prompt('Please enter filename!');
+      const blob = new Blob([multipolygon], {
+        type: 'text/plain;charset=utf-8',
+      });
+      saveAs(blob, `${filename}.wkt`);
+    };
+
+    map.setView([lat, lng], 17);
+
+    if (geojsonCircle && map.hasLayer(geojsonCircle)) {
+      map.removeLayer(geojsonCircle);
+    }
+
+    if (circleMarker && map.hasLayer(circleMarker)) {
+      map.removeLayer(circleMarker);
+    }
+
+    geojsonCircle = L.geoJSON(geojson);
+    geojsonCircle.addTo(map);
+
+    circleMarker = L.marker([lat, lng]);
+    circleMarker.addTo(map);
+
+    // var topoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png');
+    // topoMap.addTo(map);
+
+    var sat = L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}');
+    sat.addTo(map);
+  } else {
+    multipolygonContainer.innerHTML = null;
+    geojsonContainer.innerHTML = null;
+    if (map.hasLayer(geojsonCircle)) {
+      map.removeLayer(geojsonCircle);
+    }
+    if (map.hasLayer(circleMarker)) {
+      map.removeLayer(circleMarker);
+    }
+  }
 }
-
-let coord_string = '';
-fname?.addEventListener('input', function (event) {
-  const target = event.target as HTMLInputElement;
-  coord_string = target.value;
-  console.log(coord_string);
-  var lat_c: number = parseFloat(coord_string.split(',')[0]);
-  var lon_c: number = parseFloat(coord_string.split(',')[0]);
-  console.log(lat_c), lon_c;
-});
-
-// PASTE YOU COORDINATES, RADIUS and STEPS HERE
-const latlon_input: LatLngExpression = [30.520699, -81.959603];
-const radius_input = 1;
-const steps = 100;
-
-const input = {
-  lngLat: [latlon_input[1], latlon_input[0]],
-  radius: radius_input,
-};
-const options = { steps: steps, units: 'miles' };
-
-const circle = turf.circle(input.lngLat, input.radius, options);
-const geojson: GeoJSON.FeatureCollection<any> = {
-  type: 'FeatureCollection',
-  features: [circle],
-};
-const wkt: string = geojsonToWKT(geojson.features[0].geometry);
-const multipolygon = wkt
-  .replace('POLYGON ((', 'MULTIPOLYGON (((')
-  .replace('))', ')))');
-
-multipolygonContainer.innerHTML = multipolygon;
-geojsonContainer.innerHTML = JSON.stringify(geojson);
-
-save_button.onclick = () => {
-  const filename = window.prompt('Please enter filename!');
-  const blob = new Blob([multipolygon], { type: 'text/plain;charset=utf-8' });
-  saveAs(blob, `${filename}.wkt`);
-};
-
-var map = L.map('map').setView(latlon_input, 17);
-
-// console.log(JSON.stringify(geojson));
-L.geoJSON(geojson).addTo(map);
-L.marker(latlon_input).addTo(map);
-
-// var topoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png');
-// topoMap.addTo(map);
-
-var sat = L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}');
-sat.addTo(map);
